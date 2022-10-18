51d399a5c4ef4b97fcf71d382429637cd831a240
NajmeMiri <najme.miri@gmail.com>
2022-10-18 22:48:29 +0200

    Corrected the git url

--- src/footer.js
+++ src/footer.js
@@ -4,11 +4,7 @@ import "./footer.css";
 export default function Footer() {
   return (
     <footer className="footer">
-      <a
-        href="https://github.com/NajmehMiri/weather-app"
-        target="_blank"
-        rel="noreferrer"
-      >
+      <a href="https://github.com/MiriNajme" target="_blank" rel="noreferrer">
         Open-source code{" "}
       </a>
       <span>by Najmeh Miri</span>
